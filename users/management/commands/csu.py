cdb7a111374fdb5fe41e4936fca37d3f8b460d65
veniamin <venya957@gmail.com>
2024-07-23 17:04:05 +0300

    initial

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,9 +5,9 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email='admin@example.com')
+        user = User.objects.create(email='test@example.com')
         user.is_staff = True
         user.is_active = True
-        user.is_superuser = True
+        user.is_superuser = False
         user.set_password('0205')
         user.save()
